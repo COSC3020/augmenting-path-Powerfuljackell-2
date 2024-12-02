5f282c6325bd569c0a28fe152ae758a4e513fd42
powerfuljackell <johnsonjakel@aol.com>
2024-12-01 17:02:28 -0700

    Finished

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # Augmenting Paths
 
+"I certify that I have listed all sources used to complete this exercise, including the use of any Large Language Models. All of the work is my own, except where stated otherwise. I am aware that plagiarism carries severe penalties and that if plagiarism is suspected, charges may be filed against me without prior notice." 
+
+https://stackoverflow.com/questions/3559070/are-there-dictionaries-in-javascript-like-python
+https://www.w3schools.com/js/js_map_methods.asp
+https://www.geeksforgeeks.org/depth-first-search-or-dfs-for-a-graph/
+https://www.geeksforgeeks.org/find-paths-given-source-destination/
+https://stackoverflow.com/questions/8085004/iterate-through-nested-javascript-objects
+https://stackoverflow.com/questions/675231/how-do-i-access-properties-of-a-javascript-object-if-i-dont-know-the-names
+
 When we talked about the Ford-Fulkerson algorithm to find the maximum flow
 through a graph, I mentioned the "find an augmenting path" function. You're
 going to implement this function. Start with the template I provided in
@@ -32,3 +41,5 @@ accordingly.
 
 What is the worst-case big $\Theta$ complexity of your implementation? Add your
 answer, including your reasoning, to this markdown file.
+
+Given that I am performing a depth first search $\Theta$ would be $\Theta(V + E)$ as it traverses all nodes and their edges, tallying the total flow as it goes and comparing that flow to the prior best flow in order to output the highest flow path.
--- code.js
+++ code.js
@@ -1,3 +1,57 @@
 function augmentingPath(graph, start, end) {
-    return [];
+    visited = new Map
+    pathlist = []
+    pathlist.push(start)
+    final = {list: [], flow: 0}
+    if(start == end) return [start]
+    recursiveAugment(graph, visited, start, end, pathlist, 0, final)
+    return final.list;
 }
+
+
+/*
+function dfs(start, end, visited, adjlist, localpathlist){
+    if(start == end){
+        return;
+    }
+    visited[start] = true;
+    for(let i=0; i < adjlist{})
+}*/
+
+/*var graph = {'foo': {'boo': 7},
+             'boo': {'foo': 3, 'bar': 2},
+             'bar': {'boo': 4}};*/
+/*
+var graph = {'foo': {'boo': 7, 'd': 4},
+    'boo': {'foo': 3},
+    'd': {'e': 4},
+    'e': {'bar': 1},
+    'bar': {'foo': 2}};
+*/
+function recursiveAugment(graph, visited, source, sink, pathlist, flow, final){
+    if (source == sink){
+        if(final.flow < flow){
+            final.list = [...pathlist]
+            final.flow = flow
+        }
+    }
+    visited.set(source, true)
+    for (let value in graph[source]){
+        if (!(visited.has(value) && visited.get(value) == true)){
+            pathlist.push(value)
+            flow += graph[source][value]
+            recursiveAugment(graph, visited, value, sink, pathlist, flow, final)
+            pathlist.splice(pathlist.indexOf(value, 1))
+        }
+    }
+    visited.set(source, false)
+}
+
+/*
+visited = new Map
+pathlist = []
+pathlist.push('foo')
+final = {list: [], flow: 0}
+recursiveAugment(graph, visited, 'foo', 'bar', pathlist, 0, final)
+console.log(augmentingPath(graph, 'foo', 'foo'))
+*/
